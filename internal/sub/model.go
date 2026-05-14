2391d0680f2c9516d42171addf86f3894bf2f147
Babahasko <zakutni4ek@gmail.com>
2026-05-14 20:53:46 +0300

    fix model

--- internal/sub/model.go
+++ internal/sub/model.go
@@ -1,7 +1,7 @@
 package sub
 
 import (
-	"strings"
+	"fmt"
 	"time"
 
 	"github.com/google/uuid"
@@ -11,26 +11,34 @@ type MonthYear struct {
 	time.Time
 }
 
-const monthYearLayout = "01-2006"
-
-func (m *MonthYear) UnmarshalJSON(data []byte) error {
-	s := strings.Trim(string(data), `"`)
-	t, err := time.Parse(monthYearLayout, s)
-	if err != nil {
-		return err
-	}
-	m.Time = t
-	return nil
-}
-
-func (m MonthYear) MarshalJSON() ([]byte, error) {
-	return []byte(`"` + m.Time.Format(monthYearLayout) + `"`), nil
-}
-
 type Subscription struct {
-	ID          uint      `gorm:"primarykey"`
-	ServiceName string    `json:"service_name" gorm:"column:service_name"`
-	Price       int       `json:"price"`
-	UserID      uuid.UUID `json:"user_id"      gorm:"column:user_id;type:uuid"`
-	StartDate   MonthYear `json:"start_date"   gorm:"column:start_date"`
+    ID          uint      `gorm:"primarykey"`
+    ServiceName string    `json:"service_name" gorm:"column:service_name"`
+    Price       int       `json:"price"`
+    UserID      uuid.UUID `json:"user_id"      gorm:"column:user_id;type:uuid"`
+    StartDate   time.Time `json:"start_date"   gorm:"column:start_date;type:date"`
+}
+
+type SubscriptionDTO struct {
+    ServiceName string `json:"service_name"`
+    Price       int    `json:"price"`
+    UserID      string `json:"user_id"`
+    StartDate   string `json:"start_date"` // "07-2025"
+}
+
+func (dto *SubscriptionDTO) ToModel() (*Subscription, error) {
+    t, err := time.Parse("01-2006", dto.StartDate)
+    if err != nil {
+        return nil, fmt.Errorf("invalid start_date format, expected MM-YYYY")
+    }
+    uid, err := uuid.Parse(dto.UserID)
+    if err != nil {
+        return nil, fmt.Errorf("invalid user_id format")
+    }
+    return &Subscription{
+        ServiceName: dto.ServiceName,
+        Price:       dto.Price,
+        UserID:      uid,
+        StartDate:   t,
+    }, nil
 }

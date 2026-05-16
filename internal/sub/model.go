de61b4ab83bf086519c60818a8a038a0fc11f240
Babahasko <zakutni4ek@gmail.com>
2026-05-16 12:28:30 +0300

    update model with constraint in db

--- internal/sub/model.go
+++ internal/sub/model.go
@@ -1,7 +1,6 @@
 package sub
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/google/uuid"
@@ -12,33 +11,9 @@ type MonthYear struct {
 }
 
 type Subscription struct {
-    ID          uint      `gorm:"primarykey"`
-    ServiceName string    `json:"service_name" gorm:"column:service_name"`
-    Price       int       `json:"price"`
-    UserID      uuid.UUID `json:"user_id"      gorm:"column:user_id;type:uuid"`
-    StartDate   time.Time `json:"start_date"   gorm:"column:start_date;type:date"`
-}
-
-type SubscriptionDTO struct {
-    ServiceName string `json:"service_name"`
-    Price       int    `json:"price"`
-    UserID      string `json:"user_id"`
-    StartDate   string `json:"start_date"` // "07-2025"
-}
-
-func (dto *SubscriptionDTO) ToModel() (*Subscription, error) {
-    t, err := time.Parse("01-2006", dto.StartDate)
-    if err != nil {
-        return nil, fmt.Errorf("invalid start_date format, expected MM-YYYY")
-    }
-    uid, err := uuid.Parse(dto.UserID)
-    if err != nil {
-        return nil, fmt.Errorf("invalid user_id format")
-    }
-    return &Subscription{
-        ServiceName: dto.ServiceName,
-        Price:       dto.Price,
-        UserID:      uid,
-        StartDate:   t,
-    }, nil
+	ID          uint      `gorm:"primarykey"`
+	ServiceName string    `json:"service_name" gorm:"column:service_name;uniqueIndex:unique_user_service"`
+	UserID      uuid.UUID `json:"user_id"      gorm:"column:user_id;type:uuid;uniqueIndex:unique_user_service"`
+	Price       uint      `json:"price"`
+	StartDate   time.Time `json:"start_date"   gorm:"column:start_date;type:date"`
 }
